21ab0d189aedea5ebd6c6079ac8c549d17431afe
Israel Mateo Manzano <imateom76@gmail.com>
2015-10-22 21:30:57 +0200

    picture changes

--- gulpfile.js
+++ gulpfile.js
@@ -7,15 +7,15 @@ var gulp = require('gulp'),
   uncss = require('gulp-uncss');
 
 gulp.task('magiajs', function () {
-  gulp.src(['app/js/uikit.min.js', 'app/js/sticky.js', 'app/js/parallax.js', 'app/js/baguetteox.min.js'])
+  gulp.src(['app/js/uikit.min.js', 'app/js/sticky.js', 'app/js/parallax.js', 'app/js/baguetteBox.min.js'])
   .pipe(concat('todo.js'))
   .pipe(uglify())
   .pipe(gulp.dest('dist/js/'))
 });
 
 gulp.task('magiacss', function () {
-  gulp.src(['app/css/uikit.css', 'app/css/custom.css','app/css/baguetteBox.min.css'])
-  .pipe(uncss({html: ['dist/index.html']}))
+  gulp.src(['app/css/uikit.uncss.css', 'app/css/custom.css','app/css/baguetteBox.min.css'])
+  //.pipe(uncss({html: ['dist/index.html']}))
   .pipe(concat('all.css'))
   .pipe(autoprefixer())
   .pipe(minifyCSS({keepBreaks:false}))
